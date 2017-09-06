7c6c24fdc6a5042c2b019131612d00caa439c71b
Tom Everett (FGL/MARKS) <tom.everett02@C02L73KSF6T6.local>
2017-09-06 10:40:52 -0600

    example now works

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,31 @@
 
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "antlr4-runtime.h"
 #include "modelicaLexer.h"
 #include "modelicaParser.h"
 
 using namespace antlr4;
+using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+	string line;
+	ifstream modelicaFile ("example2.txt");
+	if (modelicaFile.is_open()) {
+		ANTLRInputStream input(modelicaFile);
+		modelicaLexer lexer(&input);
+		CommonTokenStream tokens(&lexer);
 
+		tokens.fill();
+		for (auto token : tokens.getTokens()) {
+			std::cout << token->toString() << std::endl;
+		}
+
+		modelicaParser parser(&tokens);
+		tree::ParseTree *tree = parser.stored_definition();
+
+		std::cout << tree->toStringTree(&parser) << std::endl;
+		modelicaFile.close();
+	}
 }
